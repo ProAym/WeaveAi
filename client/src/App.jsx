5c7218edb3d48322741e1bf25244f2e4c1eda6d9
ProAym <Aymanepro38@gmail.com>
2026-08-03 13:19:30 +0100

    feat: implement routing and authentication context with loading state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { AuthLayout, GuestLayout } from './pages/Layout'
+import AuthPage from './pages/AuthPage'
+import HomePage from './pages/HomePage'
+import BuilderPage from './pages/BuilderPage'
+import PreviewPage from './pages/PreviewPage'
+
 
 const App = () => {
   return (
-    <div>
+    <Routes>
+      {/* Login Roputes*/}
+      <Route element = {<GuestLayout />}>
+        <Route path='/login' element={<AuthPage  mode="login"/>} />
+        <Route path='/register' element={<AuthPage  mode="register"/>} />
+      </Route>
       
-    </div>
+      {/*pROTECTED ROUTES*/}
+      <Route element = {<AuthLayout />}>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/builder/:id' element={<BuilderPage/>} />
+        <Route path='/preview/:id' element={<PreviewPage/>} />
+      </Route>
+      
+    </Routes>
   )
 }
 
